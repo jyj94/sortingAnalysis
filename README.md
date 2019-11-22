d4e748a2bac6797b70826be0954f968611deb43a
jyj94 <yicho9412@gmail.com>
2019-11-22 20:29:30 +0900

    implement  file out

--- README.md
+++ README.md
@@ -1,18 +1,18 @@
 # sortingAnalysis
 
 ## 특징
-* 각각의 sorting 알고리즘을 처리 시간 횟수로 비교
+* 각각의 sorting 알고리즘을 처리 시간으로 비교
 * 선택 정렬, 삽입 정렬, 버블 정렬, 합병 정렬, 퀵 정렬
 * sorting.py를 제외한 모든 작업 최소화
 
 ## 파일 설명
 * sorting.py : main python 파일
-* boston.csv : 정렬에 사용되는 데이터셋
-* timeGraph.png : 시간 기준 비교
-* countGraph.png : 반복 횟수 기준 비교 
+* boston.csv, iris.cvs : 정렬에 사용되는 데이터셋
+* sorting.png : 시간 기준 비교
+
 
 ## 그래프
 ![Alt text](sorting.png)
 
 ## 진행상황
-2019.11.22 : 버블 정렬 구현, 선택 정렬 구현
+2019.11.22 : 버블 정렬 구현, 선택 정렬 구현, 출력 파일 생성 구현
--- sorting.py
+++ sorting.py
@@ -7,6 +7,9 @@ class data: ##데이터 저장 클래스
 
     def addData(self, inputData): ##리스트 추가 메소드
         self.data.append(inputData)
+    
+    def setData(self, inputData):
+        self.data = inputData
 
     def printData(self, i, row = 0, column = 0):
         #i = 0 : 전부 출력, i = 1 : 해당 row 출럭, i = 2 : 해당 row column 출력, i = 3 : 해당 colum 출력
@@ -34,6 +37,8 @@ class data: ##데이터 저장 클래스
                     min_idx = j
                     temp[i], temp[min_idx] = temp[min_idx], temp[i]
 
+        return temp
+
     def bubbleSort(self, column): #column은 sort 기준으로 삼을 속성 index
         temp = self.data
         dataLen = len(temp) - 1
@@ -42,22 +47,26 @@ class data: ##데이터 저장 클래스
                 if temp[j][column] > temp[j + 1][column]:
                     temp[j], temp[j+1] = temp[j+1], temp[j]
                     
-        
-    
 
 
 
 
 if __name__ == "__main__":   
 
-    csvFile = open("/programming/GitHub/sortingAnalysis/boston.csv", mode='r') ## 파일 객체 생성
+    csvFile = open("/programming/GitHub/sortingAnalysis/boston.csv", mode='r') ## 읽기 전용 파일 객체 생성
+    outFile = open("/programming/GitHub/sortingAnalysis/bostonOut.csv", mode='w') ## 쓰기 전용 파일 객체 생성
+    
+    
+    #그래프를 그리기 위해 데이터 수 각각의 sort 시간을 저장할 리스트 생성
     selectionTimeList = []
-    bubbleTimeList = [] #그래프를 그리기 위해 데이터 수 각각의 sort 시간을 저장할 리스트 생성
+    bubbleTimeList = [] 
     
 
-    ## 데이터 초기 설정
+    ## 데이터 처리 시간 비교
     bostonData = data() #데이터 객체 생성
-    csvFile.readline()  # 속성이 적혀있는 첫번째 row 읽고 버림
+    temp = csvFile.readline()  # 속성이 적혀있는 첫번째 row 읽고 버림
+    print(temp)
+    outFile.write(temp)
     for i in csvFile: 
         i = i.split(',')  #문자열을 ',' 기준으로 나눔
         temp = []
@@ -65,14 +74,16 @@ if __name__ == "__main__":
             temp.append(float(j))  #나눈 문자열을 실수(대부분의 값이 실수임)로 변경
         bostonData.addData(temp) #클래스에 저장
 
+        ## selectionSort
         startTime = time.time() #타이머 시작
-        bostonData.selectionSort(11) #12번 컬럼을 기준으로 소팅
+        bostonData.selectionSort(12) #12번 컬럼을 기준으로 소팅
         endTime = time.time() #타이머 저장
         totalTime = endTime - startTime #끝난 시간 - 시작 시간(총 걸린 시간) 
         selectionTimeList.append(totalTime)
 
+        ## bubbleSort
         startTime = time.time() #타이머 시작
-        bostonData.bubbleSort(11) #12번 컬럼을 기준으로 소팅
+        bostonData.bubbleSort(12) #12번 컬럼을 기준으로 소팅
         endTime = time.time() #타이머 저장
         totalTime = endTime - startTime #끝난 시간 - 시작 시간(총 걸린 시간) 
         bubbleTimeList.append(totalTime)
@@ -91,5 +102,9 @@ if __name__ == "__main__":
     plt.legend(loc=2)
     plt.savefig("/programming/GitHub/sortingAnalysis/sorting.png") #파일로 저장
 
-    
+    ##최종 csv 파일 생성
+    bostonData.setData(bostonData.selectionSort(12))
+    for i in bostonData.data:
+        outFile.writelines(str(i)+'\n')
+
 
